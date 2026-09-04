c91003894ddc220b31b595e158cc1727e6ee9d17
Asmi <ami.work015@gmail.com>
2026-09-04 18:27:31 +0530

    deployment

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000/api/ai/analyze";
+const API =  "https://dsa-mentor-qev6.onrender.com/api/ai/analyze";
 
 const statusEl = document.getElementById("status");
 const problemEl = document.getElementById("problem");
